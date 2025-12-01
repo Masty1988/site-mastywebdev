b0ba22344cfb4314f659d455374ba1f2ea5ce1b1
Masty1988 <masty.dev@outlook.fr>
2025-12-01 09:35:16 +0000

    valid header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
     <header className="flex w-full items-center justify-between p-6">
       <div className="flex items-center gap-4">
         <Logo className="w-16 h-auto text-blue-900" />
-              </div>
+      </div>
     </header>
   );
 }
