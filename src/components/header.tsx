8b234f692e708520bcd73efec83f8bf313530a0f
Masty1988 <masty.dev@outlook.fr>
2025-12-01 09:33:24 +0000

    fix header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,10 +5,7 @@ export default function Header() {
     <header className="flex w-full items-center justify-between p-6">
       <div className="flex items-center gap-4">
         <Logo className="w-16 h-auto text-blue-900" />
-        <span className="text-xl font-bold tracking-tight text-blue-900">
-          Masty Web Dev
-        </span>
-      </div>
+              </div>
     </header>
   );
 }
